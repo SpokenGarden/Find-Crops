527a466bd5da271441ec4cb17dfa5c072bf978ce
SpokenGarden <spokengarden@gmail.com>
2025-05-23 18:19:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -49,8 +49,7 @@ export default function GardenPlannerApp() {
       return zoneMatch && categoryMatch && sunMatch && waterMatch && soilMatch;
     });
     setFilteredCrops(matches);
-  });
-    setFilteredCrops(matches);
+  };
   };
 
   return (
