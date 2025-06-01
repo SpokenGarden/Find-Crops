31cf700b6b62cd89fed49eccf1a1128f677b5fec
SpokenGarden <spokengarden@gmail.com>
2025-06-01 14:11:57 -0700

    Add files via upload

---
+++ src/components/BackHomeButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const BackHomeButton = ({ onClick, style = {} }) => (
+  <button
+    onClick={onClick}
+    style={{
+      display: "inline-block",
+      background: "#b7e4c7",
+      color: "#155943",
+      border: "none",
+      borderRadius: "8px",
+      cursor: "pointer",
+      padding: "0.6rem 1.5rem",
+      fontSize: "1rem",
+      fontWeight: 500,
+      ...style
+    }}
+  >
+    ← Back to Home
+  </button>
+);
+
+export default BackHomeButton;
