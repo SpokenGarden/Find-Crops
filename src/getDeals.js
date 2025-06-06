2947d118c97ed0bf4fa1ab4c7600d419a17cf4e7
SpokenGarden <spokengarden@gmail.com>
2025-06-06 14:54:46 -0700

    Update getDeals.js

--- src/getDeals.js
+++ src/getDeals.js
@@ -1,5 +1,5 @@
 export async function getAmazonDeals(limit = 5) {
-  const response = await fetch(`https://deals-hlcl.onrender.com/api/deals?limit=5`);
+  const response = await fetch(`https://deals-hlcl.onrender.com/api/deals?limit=20`);
   if (!response.ok) throw new Error("Failed to fetch deals");
   const data = await response.json();
   return data.deals;
