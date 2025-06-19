24a5aa11cc6636d0ddb758f0a4d0669da9a0e8fc
SpokenGarden <spokengarden@gmail.com>
2025-06-19 16:36:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './components/CropSearchResults.css';
 import React, { useState, useEffect } from "react";
 import { filterCrops } from "./utils/filterCrops";
 import { buildSowingCalendar } from "./utils/sowingCalendar";
