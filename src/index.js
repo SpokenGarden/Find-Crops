e15c8f2429dc51068c503d54176e17fa3b420e82
SpokenGarden <spokengarden@gmail.com>
2026-01-26 10:31:39 -0800

    Update import path for GardenPlannerApp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GardenPlannerApp from "./App";
+import GardenPlannerApp from ".";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<GardenPlannerApp />);
